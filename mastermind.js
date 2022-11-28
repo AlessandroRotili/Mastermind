726195279ef1e1c5d7bc16f0a74ba5435b62b219
Raffaele_Milone <raffaele.milone@live.it>
2022-11-28 13:08:44 +0100

    JS check turn are full of colors

--- mastermind.js
+++ mastermind.js
@@ -1,17 +1,38 @@
 var colorSelected;
 var misterySequence;
+var isDuplicate = false;
 var turn = 1;
+var defaultColor;
 
 const optionsButtons = document.getElementById('options').getElementsByTagName('button');
+const tryButtons = document.getElementById('try' + turn).getElementsByTagName('button');
+const buttonCheck = document.getElementById('btn-check');
+
+const Colors = {
+	Red: "rgb(255, 0, 0)",
+	Green: "rgb(0, 128, 0)",
+	Blue: "rgb(0, 0, 255)",
+	Yellow: "rgb(255, 255, 0)",
+    Purple: "rgb(128, 0, 128)",
+    Orange: "rgb(255, 165, 0)",
+    Cyan: "rgb(0, 255, 255)",
+    Brown: "rgb(165, 42, 42)"
+}
+
+
+
+window.onload=() => {
+    defaultColor =  window.getComputedStyle(tryButtons[0]).getPropertyValue("background-color");
+}    
+
 
 for(let button of optionsButtons){
     button.onclick = ()=>{
         colorSelected = window.getComputedStyle(button).getPropertyValue("background-color");
-        console.log(colorSelected);
+            console.log(colorSelected);
     }
 }
 
-const tryButtons = document.getElementById('try' + turn).getElementsByTagName('button');
 
 for(let button of tryButtons){
     button.onclick=()=>{
@@ -20,8 +41,16 @@ for(let button of tryButtons){
     }
 }
 
+buttonCheck.onclick= ()=>{
+    for(let button of tryButtons) {
+        if (window.getComputedStyle(button).getPropertyValue("background-color") == defaultColor) {
+            alert("devi selezionare 4 colori");
+            break;
+        }
+    }    
+}
 
 
-
-const value2 = document.getElementById('');
 const value3 = document.getElementById('');
+
+
